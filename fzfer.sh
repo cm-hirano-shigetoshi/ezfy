f2a55794eaf1f8159e0ebc3ceeebfa172bdd6a92
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-04-06 14:26:08 +0900

    外から変数の注入を可能に

--- fzfer.sh
+++ fzfer.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 set -eu
 readonly YAML=$1
+shift
 
 readonly PYTHON=/usr/bin/python
 readonly TOOLDIR=$(dirname $(readlink -e $0))
-perl <(${PYTHON} ${TOOLDIR}/creator.py ${TOOLDIR}/template.pl ${YAML} | tee ~/.debug)
+set +u
+perl <( \
+  ${PYTHON} ${TOOLDIR}/creator.py ${TOOLDIR}/template.pl \
+  <(cat ${YAML} | sed \
+      -e "s\`\${1}\`$1\`g" \
+      -e "s\`\${2}\`$2\`g" \
+      -e "s\`\${3}\`$3\`g" \
+      -e "s\`\${4}\`$4\`g" \
+      -e "s\`\${5}\`$5\`g" \
+      -e "s\`\${6}\`$6\`g" \
+      -e "s\`\${7}\`$7\`g" \
+      -e "s\`\${8}\`$8\`g" \
+      -e "s\`\${9}\`$9\`g" \
+  ) | tee ~/.debug \
+)
 
