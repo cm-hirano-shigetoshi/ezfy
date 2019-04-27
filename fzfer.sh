a0208bf14b21a7018b9c6971c53c9b89a3778c49
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-04-27 14:06:00 +0900

    GNUreadlinkがない場合に対応

--- fzfer.sh
+++ fzfer.sh
@@ -3,7 +3,18 @@ set -eu
 readonly YAML=$1
 shift
 
+function readlink_e() {
+  local p
+  if ! p=$(readlink -e "$1" 2>/dev/null); then
+    p="$1"
+    while [[ -L "$p" ]]; do
+      p=$(readlink "$p")
+    done
+  fi
+  echo $p
+}
+
 readonly PYTHON=/usr/bin/python
-readonly TOOLDIR=$(dirname $(readlink -e $0))
+readonly TOOLDIR=$(dirname $(readlink_e $0))
 perl <(${PYTHON} ${TOOLDIR}/creator.py ${TOOLDIR}/template.pl ${YAML} | tee ~/.debug) "$@"
 
