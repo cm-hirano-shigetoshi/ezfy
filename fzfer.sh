d7d468ee3d0d76f321b4cf1c1ac160317969e42b
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-04-27 17:03:48 +0900

    formatterに従った

--- fzfer.sh
+++ fzfer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
-readonly SUBCMD=$1; shift
+readonly SUBCMD=$1
+shift
 
 function readlink_e() {
   local p
@@ -15,13 +16,15 @@ function readlink_e() {
 readonly TOOLDIR=$(dirname $(readlink_e $0))
 
 if [[ "${SUBCMD}" = "run" ]]; then
-  readonly YAML=$1; shift
+  readonly YAML=$1
+  shift
   perl <(python ${TOOLDIR}/creator.py ${TOOLDIR}/template.pl ${YAML}) "$@"
 elif [[ "${SUBCMD}" = "debug" ]]; then
-  readonly YAML=$1; shift
+  readonly YAML=$1
+  shift
   python ${TOOLDIR}/creator.py ${TOOLDIR}/template.pl ${YAML}
 elif [[ "${SUBCMD}" = "preview" ]]; then
-  readonly FILE=$1; shift
+  readonly FILE=$1
+  shift
   ${TOOLDIR}/preview/${FILE} "$@"
 fi
-
