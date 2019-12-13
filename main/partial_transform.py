0fa236ccfeb144383dd816f8a2788bda174bc8ae
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-12-13 11:53:17 +0900

    複数行を一気に処理する

--- main/partial_transform.py
+++ main/partial_transform.py
@@ -14,6 +14,7 @@ p.add_argument('-d', '--opt_d', action='store_true')
 args = p.parse_args()
 
 delimtier = '/'
+command = 'cat -n'
 
 def get_parts(line):
     count = 0
@@ -26,19 +27,34 @@ def get_parts(line):
             return (left, target, right)
     return (line, '', '')
 
-def shell_command(input_text, command):
+def shell_command(lines, command):
+    input_text = '\n'.join(lines)
     proc = subprocess.run(command, shell=True, input=input_text, stdout=PIPE, text=True)
-    return re.sub(r'[\r\n]$', '', proc.stdout)
+    return proc.stdout.split('\n')
+
+def flush(left, target, right):
+    transformed = shell_command(target, command)
+    for i in range(len(left)):
+        print('{}{}{}'.format(left[i], transformed[i], right[i]))
 
 try:
     line = sys.stdin.readline()
+    left_lines = []
+    target_lines = []
+    right_lines = []
     while line:
         line = line.strip('\n')
         (left, target, right) = get_parts(line)
-        transformed = shell_command(target, 'cat -n')
-        line = '{}{}{}'.format(left, transformed, right)
-        print(line)
+        left_lines.append(left)
+        target_lines.append(target)
+        right_lines.append(right)
+        if len(target_lines) > 1000:
+            flush(left_lines, target_lines, right_lines)
+            left_lines = []
+            target_lines = []
+            right_lines = []
         line = sys.stdin.readline()
+    flush()
 except BrokenPipeError:
     devnull = os.open(os.devnull, os.O_WRONLY)
     os.dup2(devnull, sys.stdout.fileno())
