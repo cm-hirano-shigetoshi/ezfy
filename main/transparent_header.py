a78ccfd2e977d693c7d575e6c51ea5a4e32ab3ad
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-12-13 17:57:17 +0900

    withで一時フォルダを消す

--- main/transparent_header.py
+++ main/transparent_header.py
@@ -16,22 +16,19 @@ try:
         line = sys.stdin.readline()
         sys.stdout.write(line)
 
-    tmpdir = tempfile.mkdtemp()
-    tmp_name = os.path.join(tmpdir, 'fifo')
-    print(tmp_name)
-    os.mkfifo(tmp_name)
-    proc = subprocess.Popen(
-        "cat {} | {}".format(tmp_name, args.command),
-        shell=True,
-        stdout=sys.stdout,
-        text=True)
-    with open(tmp_name, 'w') as f:
-        line = sys.stdin.readline()
-        while line:
-            f.write(line)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_name = os.path.join(tmpdir, 'fifo')
+        os.mkfifo(tmp_name)
+        proc = subprocess.Popen(
+            "cat {} | {}".format(tmp_name, args.command),
+            shell=True,
+            stdout=sys.stdout,
+            text=True)
+        with open(tmp_name, 'w') as f:
             line = sys.stdin.readline()
-    os.remove(tmp_name)
-    os.rmdir(tmpdir)
+            while line:
+                f.write(line)
+                line = sys.stdin.readline()
 except BrokenPipeError:
     devnull = os.open(os.devnull, os.O_WRONLY)
     os.dup2(devnull, sys.stdout.fileno())
