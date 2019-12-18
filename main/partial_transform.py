eef9abaaf75d5683026c48c3cfd2cefbe18647df
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-12-18 12:42:54 +0900

    正常に動作していなかったので修正

--- main/partial_transform.py
+++ main/partial_transform.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import re
-import time
 import subprocess
 import argparse
 import tempfile
@@ -11,8 +10,6 @@ p = argparse.ArgumentParser()
 p.add_argument('nth', type=int, help='nth')
 p.add_argument('command', help='command')
 p.add_argument('-F', '--delimiter', help='default: awk style')
-p.add_argument('--p1')
-p.add_argument('--p3')
 args = p.parse_args()
 
 if args.delimiter is not None and len(args.delimiter) > 1:
@@ -41,8 +38,8 @@ try:
                     count += 1
                     if count >= args.nth:
                         left = line[:start]
-                        target = line[start:m.start()-1]
-                        right = line[m.end()-1:]
+                        target = line[start:m.start() - 1]
+                        right = line[m.end() - 1:]
                         return (left, target, right)
                     start = m.end()
                 return (line, '', '')
@@ -73,49 +70,61 @@ try:
                 return ('', '', line)
 
     if len(args.command) > 0:
-        sys.stdout.reconfigure(line_buffering=True)
         if args.delimiter is None:
-            pattern = r'\S+'
+            pattern = re.compile(r'\S+')
         else:
-            pattern = r'[{}]'.format(args.delimiter)
+            pattern = re.compile(r'[{}]'.format(args.delimiter))
         with tempfile.TemporaryDirectory() as tmpdir:
             p1 = os.path.join(tmpdir, 'p1')
             p2 = os.path.join(tmpdir, 'p2')
-            p3 = os.path.join(tmpdir, 'p3')
             os.mkfifo(p1)
             os.mkfifo(p2)
-            os.mkfifo(p3)
             subprocess.Popen(
-                "cat {} | {} | python {} 0 '' --p1 {} --p3 {}".format(
-                    p2, args.command, __file__, p1, p3),
+                "cat {} | {} | cat -n  > {}".format(p2, args.command, p1),
+                shell=True,
+                text=True)
+            subprocess.Popen(
+                "cat {} | python {} 0 ''".format(p1, __file__),
                 shell=True,
                 stdout=sys.stdout,
                 text=True)
             with open(p1, 'w') as p1:
                 with open(p2, 'w') as p2:
-                    with open(p3, 'w') as p3:
+                    i = 0
+                    line = sys.stdin.readline()
+                    while line:
+                        i += 1
+                        parts = get_parts(line.strip('\n'), pattern)
+                        print('l:{}\t{}'.format(i, parts[0]), file=p1)
+                        print('{}'.format(parts[1]), file=p2)
+                        print('r:{}\t{}'.format(i, parts[2]), file=p1)
                         line = sys.stdin.readline()
-                        while line:
-                            parts = get_parts(line.strip('\n'), pattern)
-                            print(parts[0], file=p1)
-                            print(parts[1], file=p2)
-                            print(parts[2], file=p3)
-                            line = sys.stdin.readline()
-    elif args.p1 is not None and args.p3 is not None:
-        with open(args.p1) as p1:
-            with open(args.p3) as p3:
-                line1 = p1.readline()
-                line2 = sys.stdin.readline()
-                line3 = p3.readline()
-                while line1 and line2 and line3:
-                    line1 = line1.strip('\n')
-                    line2 = line2.strip('\n')
-                    line3 = line3.strip('\n')
-                    print('{}{}{}'.format(line1, line2, line3))
-                    line1 = p1.readline()
-                    line2 = sys.stdin.readline()
-                    line3 = p3.readline()
+    else:
+        pool = {}
+        line = sys.stdin.readline()
+        while line:
+            line = line.strip('\n')
+            if len(line) > 0:
+                p = line[0]
+                if p == 'l' or p == 'r':
+                    i = line[2:line.find('\t')]
+                else:
+                    p = 'c'
+                    line = line.lstrip()
+                    i = line[:line.find('\t')]
+                line = line[line.find('\t') + 1:]
+                if i not in pool:
+                    pool[i] = {}
+                pool[i][p] = line
+                if len(pool[i].keys()) == 3:
+                    p = pool.pop(i)
+                    print('{}{}{}'.format(p['l'], p['c'], p['r']))
+            line = sys.stdin.readline()
 except BrokenPipeError:
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    os.dup2(devnull, sys.stdout.fileno())
+    sys.exit(1)
+except KeyboardInterrupt:
     devnull = os.open(os.devnull, os.O_WRONLY)
     os.dup2(devnull, sys.stdout.fileno())
     sys.exit(1)
